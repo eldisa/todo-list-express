4182298fcd0a2bbc1db2e7f8adc7cf31f318e623
bwzhao <bwzhao@mail.fcu.edu.tw>
2022-01-08 23:50:47 +0800

    feat: add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const Todo = require('../../models/todo')
 
 router.get('/', (req, res) => {
-  Todo.find()
+  const userId = req.user._id
+  //查詢條件: userId == req.user_id
+  Todo.find({userId})  // userId: userId, 當變數名稱和屬性名稱一樣的話，可以省略後半，只留一個變數名稱。以此為例，find 條件只要填 userId 即可
     .lean()
     .sort({ _id: 'asc' }) // desc
     .then(todos => res.render('index', { todos }))
