b8fc1414bee53bfe98a7668f56228aede77cae9d
bwzhao <bwzhao@mail.fcu.edu.tw>
2022-01-09 21:47:17 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -4,35 +4,44 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const routes = require('./routes')
 
 const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
 app.use(session({
-    secret: "ThisIsMySecret",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true
 }))
-app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+
 app.use(flash())
-app.use((req, res, next)=>{
-    res.locals.isAuthenticated =req.isAuthenticated()
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.isAuthenticated()
     res.locals.user = req.user
-    res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
-    res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.warning_msg = req.flash('warning_msg')
     next()
 })
+
 app.use(routes)
 
 app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`)
+    console.log(`App is running on http://localhost:${PORT}`)
 })
